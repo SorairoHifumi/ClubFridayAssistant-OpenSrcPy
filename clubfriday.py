bcd7a60ceb21f7ee9a9ed88cc6965ec11b55da9e
WasdMstr <86450401+SorairoHifumi@users.noreply.github.com>
2021-09-17 14:16:35 +0700

    Update clubfriday.py

--- clubfriday.py
+++ clubfriday.py
@@ -1,9 +1,10 @@
 import discord
 import os
 from discord.ext import commands
+from discord.voice_client import VoiceClient
 import datetime
 import asyncio
-
+token = 'intentionally left out' #put token here
 colorlib = {
 'yellow' : 0xffff8a,
 'red' :0xff9999,
@@ -15,57 +16,74 @@ helpIntro = discord.Embed(title = 'Help Page',color = 0xffffff)
 helpIntro.add_field(name='Introduction:',value="""The commands that are listed here is the command that is available to the highest rank
 If the server owner decided to unload some script, then you wouldn't be able to use some
 of the commands, specifically the ones that are in the script that got unloaded. The bot is loaded with 
-every script by default.""")#Hello are you here?
+every script by default.""")
 
 helpEmbed = [helpIntro,
 
 discord.Embed(title='General Commands',color=colorlib['red'],description="""
-`<reload`
+`<reloadall`
 Reload all scripts.
-
-`<load`
+`<reload [script]`
+Reload all scripts.
+`<load [script]`
 Load specific script.
-
-`<unload`
+`<unload [script]`
 Unload specific script.
-
 `<version`
 Get the version that the bot is run on.
-
 `<ping`
 Get the latency between Discord server and the bot server.
-
+`<join`
+Get the bot to connect to the voice channel.
+`<leave`
+Get the bot to disconnect from the voice channel.
 """)
 ,
 discord.Embed(title='Schedule Commands',color=colorlib['orange'],description="""
-`<c [Class] [period]`
+`<c [Grade] [Class] [period]`
 Get the specified class today
-
-`<ctmr [Class] [period]`
+`<ctmr [Grade] [Class] [period]`
 Get the specified class tomorrow
-
 `<db`
 Look at the database that the bot is using.
-
-`<hr`
+`<hr [Grade] [Class]`
 Get the link to be redirected to homeroom.
-
-`<call [Class] [Day of the week]`
+`<call [Grade] [Class] [Day of the week]`
 List all the class in the day specified
-
-`<cindiv [Dayof the week] [Class] [Period]`
+`<cindiv [Dayof the week] [Grade] [Class] [Period]`
 Get a specific class in a specific day.
-
-`<cinfo [Class] [Subject Name]`aaaaaaaaaaaa
+`<cinfo [Grade] [Class] [Subject Name]`
 Get Info about the Subject.
-
-
 """),
+discord.Embed(title='Schedule Commands',color=colorlib['orange'],description="""
+Time Table Reading Instruction:
+`___` 
+Means the class is not presented.
+`[X]` 
+Means the class is presented.
+`<X>` 
+Means this class is the class you called and the class is presented.
+`[X= =X]` or `[X= =X= =X]` 
+Means connected class (Double, Triple class).
+""")
+,
 discord.Embed(title='Fortune Cookie! Command',color=colorlib['yellow'],description="""
 `<cookie`
 Eat a fortune cookie.
 `<cookiedelay [seconds]`
-Set `<cookie` delay between message.
+Set `<cookie` message delay.
+"""),
+discord.Embed(title='Calculator Commands',color=colorlib['green'],description="""
+`<calc [a] [+,-,*,/,^] [b]`
+Do a simple calculation.
+`<sqrt [number]`
+Get the square root of a number.
+`<circle [radius]`
+Get some information of a circle (Circumference and Area).
+`<trig [sin/cos/tan] [angle(deg)]`
+Get the sine,cosine,tangent of an angle.
+`<setrounding [int]`
+Set the rounding for decimal rounding.
 """)
 ]
 
@@ -75,8 +93,12 @@ async def on_ready():
   print('Main Loaded!')
 
 for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
+    print(f'Trying {filename}...')
+    if filename.startswith('_'):
+      print(f'Ignoring {filename}')
+    elif filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+        print(f'{filename} Loded!')
 
 @client.command()
 async def help(ctx):
@@ -117,6 +139,14 @@ async def load(ctx,ext):
     except:
         await ctx.send('There is no such script.')
 @client.command()
+async def reload(ctx,ext):
+    try:
+        client.unload_extension(f'cogs.{ext}')
+        client.load_extension(f'cogs.{ext}')
+        await ctx.send(f'Reloaded {ext}.py')
+    except:
+        await ctx.send('There is no such script.')
+@client.command()
 async def unload(ctx,ext):
     try:
         client.unload_extension(f'cogs.{ext}')
@@ -125,15 +155,19 @@ async def unload(ctx,ext):
         await ctx.send('Failed!')
         
 @client.command()
-async def reload(ctx):
+async def reloadall(ctx):
     i = 0
+    count = len(os.listdir('./cogs'))
     for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            client.unload_extension(f'cogs.{filename[:-3]}')
-            client.load_extension(f'cogs.{filename[:-3]}')
-            await ctx.send(f'Reloaded {filename}')
-            i += 1
-    await ctx.send(f'{i} script(s) has been reloaded in total.')
+      if filename.startswith('_'):
+        continue
+ 
+      if filename.endswith('.py'):
+          client.unload_extension(f'cogs.{filename[:-3]}')
+          client.load_extension(f'cogs.{filename[:-3]}')
+          await ctx.send(f'Reloaded {filename}')
+          i += 1
+    await ctx.send(f'{i} script(s) has been reloaded out of {count} files')
     
 @client.command()
 async def ping(ctx):
@@ -146,13 +180,18 @@ async def getservertime(ctx):
     await ctx.send(datetime.datetime.now())
     await ctx.send(dotw)
 
+@client.command()
+async def join(ctx):
+  channel = ctx.author.voice.channel
+  await channel.connect()
+
+@client.command()
+async def leave(ctx):
+  await ctx.voice_client.disconnect()
 
 @client.command()
 async def version(ctx):
-	await ctx.send('Running Version : v1.2.5 Server 2')
+	await ctx.send('Running Version : v1.3.3 Serv2')
 
-@client.command()
-async def nuay(ctx):
-	await ctx.send('nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay nuay',tts=True)
 
-client.run('no')
+client.run(token)
